093fda85d60832aca6539433f145e16455210be7
Yijun Gai <ethan.yijun@gmail.com>
2024-01-29 22:42:12 +1100

    Fix

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -3,10 +3,11 @@ import axios from "axios";
 export default ({ req }: { req: any }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
+    console.log("We are on the server");
     return axios.create({
       baseURL: "http://www.ethangai.xyz",
-      headers: { ...req.headers, Host: "ticketing.dev" },
+      //{ ...req.headers, Host: "ticketing.dev" },
+      headers: req.headers,
     });
   } else {
     // We must be on the browser
